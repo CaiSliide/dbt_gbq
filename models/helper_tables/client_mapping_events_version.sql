b915a29c0dae2b0f14411633ebdf49c9e8122440
Cai Parry-Jones <cai.pj@sliide.com>
2023-12-21 11:29:49 +0000

    complete dau, mau, wau KPIs and change client_mapping to include one view for all clients

--- models/helper_tables/client_mapping_events_version.sql
+++ models/helper_tables/client_mapping_events_version.sql
@@ -1,5 +1,14 @@
 with
-    base as (
+    clients_not_in_client_mapping_events as (
+        select
+            app_package_name, client, flavour, product, sliide_percentage, partner_code
+        from {{ ref("client_mapping") }}
+        where
+            app_package_name
+            not in (select app_package_name from {{ ref("client_mapping_events") }})
+
+    ),
+    client_mapping_events as (
         select
             app_package_name,
             partner_code,
@@ -53,14 +62,31 @@ with
                 || lpad(split(version_to, '.')[offset(2)], 3, '0') as integer
             ) as version_to_num_long
         from {{ ref("client_mapping_events") }}
+    ),
+    client_mapping_non_events as (
+        select
+            app_package_name,
+            partner_code,
+            product,
+            client,
+            flavour,
+            '00.00.00' as version_from_txt_short,
+            '99.99.99' as version_to_txt_short,
+            '000.000.000' as version_from_txt_long,
+            '999.999.999' as version_to_txt_long,
+            safe_cast('0' as integer) as version_from_num_short,
+            safe_cast('999999' as integer) as version_to_num_short,
+            safe_cast('0' as integer) as version_from_num_long,
+            safe_cast('999999999' as integer) as version_to_num_long
+        from clients_not_in_client_mapping_events
     )
 
 select
     app_package_name,
-    max(partner_code) as partner_code,
-    max(product) as product,
-    max(client) as client,
-    max(flavour) as flavour,
+    partner_code,
+    product,
+    client,
+    flavour,
     version_from_txt_short,
     version_to_txt_short,
     version_from_txt_long,
@@ -69,5 +95,22 @@ select
     version_to_num_short,
     version_from_num_long,
     version_to_num_long
-from base
-group by 1, 6, 7, 8, 9, 10, 11, 12, 13
+from client_mapping_events
+
+union all
+
+select
+    app_package_name,
+    partner_code,
+    product,
+    client,
+    flavour,
+    version_from_txt_short,
+    version_to_txt_short,
+    version_from_txt_long,
+    version_to_txt_long,
+    version_from_num_short,
+    version_to_num_short,
+    version_from_num_long,
+    version_to_num_long
+from client_mapping_non_events
