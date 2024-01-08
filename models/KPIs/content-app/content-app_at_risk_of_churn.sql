fb3ecbe96a5ee97041cd26a7f181df0d236b7c8a
Cai Parry-Jones <cai.pj@sliide.com>
2024-01-08 11:39:36 +0000

    working on at risk of churn kpi

--- models/KPIs/content-app/content-app_at_risk_of_churn.sql
+++ models/KPIs/content-app/content-app_at_risk_of_churn.sql
@@ -1,22 +1,30 @@
 with
-    base as (
-        select distinct
+    base_0_to_28_day_users as (
+        select
             user_pseudo_id,
-            session_start_date,
-            app_family_name as app_family_name,
+            app_family_name,
+            app_package_name,
+            app_version_string,
+            device_model,
+            device_os_version,
+            max(session_start_date) as max_date
+        from {{ ref("content-app_sessions") }}
+        where
+            is_active_session is true
+            and session_start_date
+            between date_sub(current_date(), interval 28 day) and current_date()
+        group by
+            user_pseudo_id,
+            app_family_name,
             app_package_name,
             app_version_string,
             device_model,
             device_os_version
-        from {{ ref("content-app_sessions") }}
-        where
-            is_active_session is true
-            and session_start_date between '2023-10-01' and '2023-10-30'
     )
 
 -- By Flavour breakdown
 select
-    DATE_SUB(base.session_start_date, INTERVAL 6 DAY) as week_start,
+    date_sub(base.session_start_date, interval 6 day) as week_start,
     base.session_start_date as week_end,
     base.app_family_name as product,
     base.app_package_name as flavour,
@@ -33,14 +41,13 @@ join
     and base.app_family_name = b2.app_family_name
     and base.app_package_name = b2.app_package_name
     and base.user_pseudo_id = b2.user_pseudo_id
-where
-    EXTRACT(DAYOFWEEK FROM base.session_start_date) = 1
+where extract(dayofweek from base.session_start_date) = 1
 group by base.session_start_date, base.app_family_name, base.app_package_name
 
- -- By Flavour & Version breakdown
+-- By Flavour & Version breakdown
 union all
 select
-    DATE_SUB(base.session_start_date, INTERVAL 6 DAY) as week_start,
+    date_sub(base.session_start_date, interval 6 day) as week_start,
     base.session_start_date as week_end,
     base.app_family_name as product,
     base.app_package_name as flavour,
@@ -58,14 +65,17 @@ join
     and base.app_package_name = b2.app_package_name
     and base.user_pseudo_id = b2.user_pseudo_id
     and base.app_version_string = b2.app_version_string
-where
-    EXTRACT(DAYOFWEEK FROM base.session_start_date) = 1
-group by base.session_start_date, base.app_family_name, base.app_package_name, base.app_version_string
+where extract(dayofweek from base.session_start_date) = 1
+group by
+    base.session_start_date,
+    base.app_family_name,
+    base.app_package_name,
+    base.app_version_string
 
 -- By Flavour & Version & Device breakdown
-    union all
+union all
 select
-    DATE_SUB(base.session_start_date, INTERVAL 6 DAY) as week_start,
+    date_sub(base.session_start_date, interval 6 day) as week_start,
     base.session_start_date as week_end,
     base.app_family_name as product,
     base.app_package_name as flavour,
@@ -84,13 +94,17 @@ join
     and base.user_pseudo_id = b2.user_pseudo_id
     and base.app_version_string = b2.app_version_string
     and base.device_model = b2.device_model
-where
-    EXTRACT(DAYOFWEEK FROM base.session_start_date) = 1
-group by base.session_start_date, base.app_family_name, base.app_package_name, base.app_version_string, base.device_model
+where extract(dayofweek from base.session_start_date) = 1
+group by
+    base.session_start_date,
+    base.app_family_name,
+    base.app_package_name,
+    base.app_version_string,
+    base.device_model
 -- By Flavour & Version & OS breakdown
 union all
 select
-    DATE_SUB(base.session_start_date, INTERVAL 6 DAY) as week_start,
+    date_sub(base.session_start_date, interval 6 day) as week_start,
     base.session_start_date as week_end,
     base.app_family_name as product,
     base.app_package_name as flavour,
@@ -109,6 +123,10 @@ join
     and base.user_pseudo_id = b2.user_pseudo_id
     and base.app_version_string = b2.app_version_string
     and base.device_os_version = b2.device_os_version
-where
-    EXTRACT(DAYOFWEEK FROM base.session_start_date) = 1
-group by base.session_start_date, base.app_family_name, base.app_package_name, base.app_version_string, base.device_os_version
+where extract(dayofweek from base.session_start_date) = 1
+group by
+    base.session_start_date,
+    base.app_family_name,
+    base.app_package_name,
+    base.app_version_string,
+    base.device_os_version
