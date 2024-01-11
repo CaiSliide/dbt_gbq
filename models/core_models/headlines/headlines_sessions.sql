b5329d591a96011cbd623d4e56c91292c26bb2dd
Cai Parry-Jones <cai.pj@sliide.com>
2024-01-11 09:16:28 +0000

    headlines core models finished

--- models/core_models/headlines/headlines_sessions.sql
+++ models/core_models/headlines/headlines_sessions.sql
@@ -69,7 +69,7 @@ with base as (
                 event_date between (current_date() - 3) and current_date()
         {% else %}
             where
-                event_date between '2023-10-01' and '2023-10-31'
+                event_date between '2023-09-01' and '2023-12-31'
         {% endif %}
 
     group by
